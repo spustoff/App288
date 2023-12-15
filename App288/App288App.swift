117aa7aa647a2299fb3b2df9ea7668068473bcf3
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2023-12-15 13:00:49 +0300

    Initial Commit

---
+++ App288/App288App.swift
@@ -0,0 +1,17 @@
+//
+//  App288App.swift
+//  App288
+//
+//  Created by IGOR on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct App288App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
